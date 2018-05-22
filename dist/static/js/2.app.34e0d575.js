ca37a35f1fcd54c1e69d5b6fa1f4547b2af9bc91
Jeremie Drazic <jeremie.drazic@disko.fr>
2018-05-22 14:17:31 +0200

    added ./dist to .gitignore

--- dist/static/js/2.app.34e0d575.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"+QRz":function(n,e,o){n.exports=o.p+"static/img/bunny.671dfaa9.png"},Wa5j:function(n,e,o){},tjUo:function(n,e,o){"use strict";o.r(e);var a=o("weu1"),t=(o("Wa5j"),o("+QRz")),c=o.n(t),i=o("q1tI"),d=o.n(i),p=o("i8i4"),s=o.n(p);document.body.appendChild(function(){var n=document.createElement("div");n.innerHTML=Object(a.a)(["Hello","Webpack"]," "),n.classList.add("hello");var e=new Image;return e.src=c.a,n.appendChild(e),console.log(d.a),console.log(s.a),n}())}},[["tjUo",0,1]]]);
-//# sourceMappingURL=2.app.34e0d575.js.map
