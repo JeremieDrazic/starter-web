c94a86cf35389546c0bb2066b60b02e712c1bb47
Jeremie Drazic <jeremie.drazic@disko.fr>
2018-06-02 19:17:48 +0200

    added stylelint config

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
+const processorExtension = require('./build/config').cssPreprocessorExtension
+
 module.exports = {
+  syntax: processorExtension === 'scss' ? 'postcss-scss' : undefined,
   plugins: [
     require('autoprefixer')
   ]
